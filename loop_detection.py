cc8d7c3fba29790b63582d6b1b0dc35557f72d41
mikevin <kevin.michot@ynov.com>
2025-10-02 14:11:00 +0200

    loop

---
+++ loop_detection.py
@@ -0,0 +1,14 @@
+from cell_state import next_generation
+
+def state_key(grid):
+    return tuple(tuple(row) for row in grid)
+
+def detect_loop(seen, grid, turn):
+    key = state_key(grid)
+    if key in seen:
+        start = seen[key]
+        period = turn - start
+        return True, start, period
+    else:
+        seen[key] = turn
+        return False, None, None
